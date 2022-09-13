a9de95ed2ad42224e71912ffd174b5df02d08680
mimegil556 <113430069+mimegil556@users.noreply.github.com>
2022-09-13 15:11:46 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import "./App.css";
-import Header from "./components/Header";
+https://twitter.com/ManotoNews/status/1569623179198607361
 import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Cart from "./components/Cart";
 
+function App() {
+  return (
+    import "./App.css";
+import Header from "./components/Header";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+
+function App() {
+  return (
 function App() {
   return (
     <BrowserRouter>
